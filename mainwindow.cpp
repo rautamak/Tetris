f9bc6d6bd52ef0bb0171ca199889f8b7edd9a6ef
rautamak <timi.rautamaki@gmail.com>
2020-05-07 17:19:58 +0300

    Tetris: initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,74 @@
+#include "mainwindow.hh"
+#include "ui_mainwindow.h"
+#include <QColor>
+#include <QKeyEvent>
+#include <QGraphicsRectItem>
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow) {
+
+    ui->setupUi(this);
+
+    // We need a graphics scene in which to draw rectangles.
+    scene_ = new QGraphicsScene(this);
+
+    int left_margin = 100; // x coordinate
+    int top_margin = 150; // y coordinate
+
+    ui->graphicsView->setGeometry(left_margin, top_margin,
+                                  BORDER_RIGHT + 2, BORDER_DOWN + 2);
+    ui->graphicsView->setScene(scene_);
+
+    scene_->setSceneRect(0, 0, BORDER_RIGHT - 1, BORDER_DOWN - 1);
+
+    // Setting random engine ready for the first real call.
+    int seed = time(0); // You can change seed value for testing purposes
+    randomEng.seed(seed);
+    distr = std::uniform_int_distribution<int>(0, NUMBER_OF_TETROMINOS - 1);
+    distr(randomEng);
+
+    // Start game
+    game();
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
+
+void MainWindow::keyPressEvent(QKeyEvent* event) {
+
+/*    // Check that Tetrino is created and correct size
+    if ( current_ == nullptr || current_->size() != 4 ) {
+        return;
+    }
+
+    if ( event->key() == Qt::Key_Down || event->key() == Qt::Key_S ) {
+        fall_block(true);
+    }
+
+    if ( event->key() == Qt::Key_Left || event->key() == Qt::Key_A ) {
+        move_block(LEFT);
+    }
+
+    if ( event->key() == Qt::Key_Right || event->key() == Qt::Key_D ) {
+        move_block(RIGHT);
+    }
+
+    if ( event->key() == Qt::Key_Space ) {
+        move_to_bottom();
+    }*/
+}
+
+void MainWindow::gameloop() {
+
+}
+
+void MainWindow::game() {
+
+    // Set up timer and start game loop
+    timer_.setSingleShot(false);
+    connect(&timer_, &QTimer::timeout, this, &MainWindow::gameloop);
+    timer_.start(1000);
+}
---
+++ mainwindow.hh
@@ -0,0 +1,84 @@
+#ifndef MAINWINDOW_HH
+#define MAINWINDOW_HH
+
+#include <QMainWindow>
+#include <QGraphicsScene>
+#include <QTimer>
+#include <random>
+
+//using Tetromino = std::vector< QGraphicsRectItem* >;
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    bool DEBUG = true;
+    Ui::MainWindow *ui;
+
+    QGraphicsScene* scene_;
+
+    // Constants describing scene coordinates
+    const int BORDER_UP = 0;
+    const int BORDER_DOWN = 480;
+    const int BORDER_LEFT = 0;
+    const int BORDER_RIGHT = 240;
+
+    const int SQUARE_SIDE = 20;
+    const int COLUMNS = BORDER_RIGHT / SQUARE_SIDE;
+    const int ROWS = BORDER_DOWN / SQUARE_SIDE;
+
+
+    // How much to move a block per frame
+    const int SPEED = SQUARE_SIDE;
+    // How much to move a block on keypress
+    const int KEYPRESS_SPEED = 2 * SPEED;
+    // How much to move a block sideways on keypress
+    const int LATERAL_SPEED = SQUARE_SIDE;
+
+    // Constants for different tetrominos and the number of them
+    enum Tetromino_kind { HORIZONTAL,
+                          LEFT_CORNER,
+                          RIGHT_CORNER,
+                          SQUARE,
+                          STEP_UP_RIGHT,
+                          PYRAMID,
+                          STEP_UP_LEFT,
+                          NUMBER_OF_TETROMINOS };
+
+    enum DIRECTIONS { LEFT, RIGHT, DOWN };
+
+    // For randomly selecting the next dropping tetromino
+    std::default_random_engine randomEng;
+    std::uniform_int_distribution<int> distr;
+
+    // Game timer
+    QTimer timer_;
+
+    /**
+     * @brief keyPressEvent
+     * @param event
+     * Track keyboard events
+     */
+    void keyPressEvent(QKeyEvent* event);
+    /**
+     * @brief gameloop
+     * Main loop of the game
+     */
+    void gameloop();
+    /**
+     * @brief game
+     * Set up variables to begin gameloop
+     */
+    void game();
+};
+
+#endif // MAINWINDOW_HH
